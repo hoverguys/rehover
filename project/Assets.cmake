87a2883d762c69cf79ee8e25a628bf20b99c92e9
Hamcha <zikyky@gmail.com>
2017-11-29 23:48:44 +0100

    Add test map

--- project/Assets.cmake
+++ project/Assets.cmake
@@ -4,11 +4,12 @@ set(ASSETS
 		models/hovercraft.obj
 		models/plane.obj
 		models/terrain.obj
+		models/testmap.obj
 	TEXTURE
 		IA8 CLAMP  BILINEAR textures/hovercraftGlobal.png
 		IA8 CLAMP  BILINEAR textures/hovercraftShade.png
 		I4  REPEAT NEAR     textures/checkerboard.png
-		RGB565 REPEAT BILINEAR textures/terrain.png
+		RGB565 REPEAT BILINEAR textures/testmap.png
 	BIN
 		shaders/hovercraft.bin
 )
